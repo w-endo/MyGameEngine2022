311980de2aab05cd65143b6321daa016fe0ddbc4
w.endo <w.endo@E0404-50>
2022-04-25 10:15:28 +0900

    ウィンドウプロシージャ作成

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+//インクルード
+#include <Windows.h>
+
+//プロトタイプ宣言
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//エントリーポイント
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウクラス（設計図）を作成
+
+	//ウィンドウを作成
+
+	//ウィンドウを表示
+
+	//メッセージループ（何か起きるのを待つ）
+
+	return 0;
+}
+
+//ウィンドウプロシージャ（何かあった時によばれる関数）
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);  //プログラム終了
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
