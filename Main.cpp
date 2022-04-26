cdfa2ce1c6b2d2fa0f7df726cd0e5bb40fe1fb81
航 遠藤 <w.endo@jc-21.jp>
2022-04-26 14:32:17 +0900

    ウィンドウ出た

--- Main.cpp
+++ Main.cpp
@@ -8,13 +8,59 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
 	//ウィンドウクラス（設計図）を作成
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
+	wc.hInstance = hInstance;                   //インスタンスハンドル
+	wc.lpszClassName = L"SampleGame";            //ウィンドウクラス名
+	wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
+	wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
+	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
+	wc.lpszMenuName = NULL;                     //メニュー（なし）
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //背景（白）
+	RegisterClassEx(&wc); //クラスを登録
 
 	//ウィンドウを作成
+	HWND hWnd = CreateWindow(
+		L"SampleGame",         //ウィンドウクラス名
+		L"サンプルゲーム",     //タイトルバーに表示する内容
+		WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
+		CW_USEDEFAULT,       //表示位置左（おまかせ）
+		CW_USEDEFAULT,       //表示位置上（おまかせ）
+		800,                 //ウィンドウ幅
+		600,                 //ウィンドウ高さ
+		NULL,                //親ウインドウ（なし）
+		NULL,                //メニュー（なし）
+		hInstance,           //インスタンス
+		NULL                 //パラメータ（なし）
+	);
 
 	//ウィンドウを表示
+	ShowWindow(hWnd, nCmdShow);
 
 	//メッセージループ（何か起きるのを待つ）
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
+	{
+		//メッセージあり
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 
+		//メッセージなし
+		else
+		{
+			//ゲームの処理
+
+
+		}
+	}
 	return 0;
 }
 
